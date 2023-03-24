7418f80313a4db34a2424a5134b03fed1512b19a
PedroHamamoto <hamamoto.pedro@gmail.com>
2023-03-23 23:30:00 -0300

    Implementing Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/br/com/hamamoto/binary_tree/TreeBuilder.java
@@ -0,0 +1,55 @@
+package br.com.hamamoto.binary_tree;
+
+
+import java.util.Arrays;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ *<a href="https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/"> LeetCode problem </a>
+ * - Construct Binary Tree from Preorder and Inorder Traversal
+ * */
+class TreeBuilder {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0)
+            return null;
+
+        var root = new TreeNode(preorder[0]);
+
+        int mid = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (root.val == inorder[i]) {
+                mid = i;
+                break;
+            }
+        }
+
+        root.left = buildTree(
+                Arrays.copyOfRange(preorder, 1, mid + 1),
+                Arrays.copyOfRange(inorder, 0, mid)
+        );
+        root.right = buildTree(
+                Arrays.copyOfRange(preorder, mid + 1, preorder.length),
+                Arrays.copyOfRange(inorder, mid + 1, inorder.length)
+        );
+
+        return root;
+    }
+}
