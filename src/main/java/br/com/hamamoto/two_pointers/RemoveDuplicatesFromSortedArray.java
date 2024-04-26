56b0197de9454f1f7f39f254af9f3cb3bbe4f238
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-04-25 17:07:29 -0700

    Repacking

---
+++ src/main/java/br/com/hamamoto/two_pointers/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+package br.com.hamamoto.two_pointers;
+public class RemoveDuplicatesFromSortedArray {
+
+    /**
+     *<a href="https://leetcode.com/problems/q"> LeetCode problem </a>
+     * - Remove Duplicates  from Sorted Array
+     * */
+    public int removeDuplicates(int[] nums) {
+        int left = 1;
+
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[right] != nums[right - 1]) {
+                nums[left++] = nums[right];
+            }
+        }
+
+        return left;
+    }
+
+}
