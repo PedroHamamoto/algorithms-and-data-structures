ead31db3df30308776fd06bd416cf0c296ac0fbf
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-04-28 12:03:38 -0700

    Implementing Two Sum II

--- src/main/java/br/com/hamamoto/two_pointers/TwoSumII.java
+++ src/main/java/br/com/hamamoto/two_pointers/TwoSumII.java
@@ -5,4 +5,23 @@ package br.com.hamamoto.two_pointers;
  * - Two Sum II - Input Array Is Sorted
  */
 public class TwoSumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+        var left = 0;
+        var right = numbers.length - 1;
+        var sum = numbers[left] + numbers[right];
+
+        while (sum != target) {
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+
+            sum = numbers[left] + numbers[right];
+        }
+
+        return new int[] {left + 1, right + 1};
+    }
+
 }
