b18481f104c0014d3d5d5ae5593fd264bcdf5d19
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-04-30 10:44:37 -0700

    Implementing Cont Complete Tree Nodes

--- src/main/java/br/com/hamamoto/binary_tree/CountCompleteTreeNodes.java
+++ src/main/java/br/com/hamamoto/binary_tree/CountCompleteTreeNodes.java
@@ -5,4 +5,12 @@ package br.com.hamamoto.binary_tree;
  * - Count Complete Tree Nodes
  */
 public class CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
 }
