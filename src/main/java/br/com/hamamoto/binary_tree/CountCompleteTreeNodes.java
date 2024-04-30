4ae8b1dfa8eebad2e1a2540a6bcb9ac88d872682
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-04-30 10:37:30 -0700

    Implementing Cont Complete Tree Nodes

---
+++ src/main/java/br/com/hamamoto/binary_tree/CountCompleteTreeNodes.java
@@ -0,0 +1,8 @@
+package br.com.hamamoto.binary_tree;
+
+/**
+ * <a href="https://leetcode.com/problems/count-complete-tree-nodes> LeetCode problem </a>
+ * - Count Complete Tree Nodes
+ */
+public class CountCompleteTreeNodes {
+}
